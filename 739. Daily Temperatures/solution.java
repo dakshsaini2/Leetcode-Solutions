adc09525b7f40e0298dc20041beeefb6e41a4dd7
dakshsaini2 <dakshsaini200523@gmail.com>
2026-05-05 22:14:19 +0530

    Create solution.java

---
+++ 739. Daily Temperatures/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
+                int idx = st.pop();
+                res[idx] = i - idx;
+            }
+            st.push(i);
+        }
+
+        return res;
+    }
+}
